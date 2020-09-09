abdf0fcdac7f660381be302a99043ab26ca7c592
amanpra123 <amanpra333@gmail.com>
2020-09-09 15:35:00 +0530

    conflicts sloved

--- tests/task.test.js
+++ tests/task.test.js
@@ -2,13 +2,15 @@ const request = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/users')
 const Task = require('../src/models/tasks')
-const {taskOne,userOne}= require('./fixtures/db')
+const {taskOne,userOne,setUpData}= require('./fixtures/db')
+
+beforeEach(setUpData)
 
 test('get Task',async()=>{
     await request(app)
     .get('/tasks')
     .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
     .send()
-    .expect(200)
+    .expect(201)
     // expect(response.body.length).toEqual(1)
 })
